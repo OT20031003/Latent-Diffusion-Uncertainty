a7a730daee97f7bb111dd1f1e8a11af80e357fde
OT20031003 <ono1003takuma@gmail.com>
2025-12-26 09:37:21 +0900

    log run.sh

---
+++ run.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# 仮想環境のアクティベート（必要なら）
+# source /path/to/venv/bin/activate
+
+# 1つずつ実行するため、行末の '&' と行頭の 'nohup' は削除します
+# ログファイル名を変えておくと後で分析しやすいです
+
+echo "--- Job 1 start ---"
+python -m scripts.img2img --snr -5 -r 0.2 > log_snr-5_r0.2.txt 2>&1
+
+echo "--- Job 2 start ---"
+python -m scripts.img2img --snr -10 -r 0.2 > log_snr-10_r0.2.txt 2>&1
+
+echo "--- Job 3 start ---"
+python -m scripts.img2img --snr -7 -r 0.2 > log_snr-7_r0.2.txt 2>&1
+
+echo "--- Job 4 start ---"
+python -m scripts.img2img --snr -5 -r 0.1 > log_snr-5_r0.1.txt 2>&1
+
+echo "--- Job 5 start ---"
+python -m scripts.img2img --snr -10 -r 0.1 > log_snr-10_r0.1.txt 2>&1
+
+echo "--- Job 6 start ---"
+python -m scripts.img2img --snr -10 -r 0.3 > log_snr-10_r0.3.txt 2>&1
+
+echo "--- Job 7 start ---"
+python -m scripts.img2img --snr 0 -r 0.1 > log_snr-0_r0.1.txt 2>&1
+
+echo "--- Job 8 start ---"
+python -m scripts.img2img --snr 0 -r 0.2 > log_snr-0_r0.2.txt 2>&1
+
+echo "complete"
